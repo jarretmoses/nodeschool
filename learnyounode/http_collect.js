14bc357728ba814191654cdb937a7f98b7928de3
Jarret Moses <jarret.moses@gmail.com>
2015-01-25 18:39:08 -0500

    http connection done

---
+++ learnyounode/http_collect.js
@@ -0,0 +1,13 @@
+var http = require('http');
+var url = process.argv[2];
+var bl = require('bl');
+
+
+
+http.get(url, function(res){
+  res.pipe(bl(function(err,data){
+  if(err) return console.error(err);
+    console.log(data.length);
+    console.log(data.toString());
+  }));
+});
